0e5d8ae56ab1df3d4817579467060974c4000f06
Trishen <trishendragon@yahoo.com>
2020-11-11 00:07:19 +0200

    added murals

---
+++ DGSappSem2/DGSappSem2/Models/Extramural/Mural.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace DGSappSem2.Models.Extramural
+{
+    public class Mural
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int MuralId { get; set; }
+        [DisplayName("Mural Name")]
+        public string Title { get; set; }
+    }
+}
